5d9480631415ca8e40655b62430bfa12bbd752ff
Michał Marciniak <michal.mar725@gmail.com>
2025-10-12 18:01:28 +0200

    add more food

--- src/foodFactory.js
+++ src/foodFactory.js
@@ -9,6 +9,7 @@ export default function foodFactory() {
     foodArr.push(new Food('Cherries', 'Fruitarian', 150, 'Minor-fruit-cherry-96x96.png'));
     foodArr.push(new Food('Pastry', 'Castlevaniesque', 300, 'Minor-castle-cake-96x96.png'));
     foodArr.push(new Food('"Le Croissant"', 'Baguette', 200, 'Minor-bag-croissant-96x96.png'));
+    foodArr.push(new Food('Small medkit', 'Half-Life', 500, 'Smallmedkit.png'));
     foodArr.push(new Food('Gruyère', 'Cheese', 150, 'Minor-cheese-wedge.png'));
 
     let minorFoods = new FoodSection('Minor foods', foodArr);
@@ -19,6 +20,7 @@ export default function foodFactory() {
     foodArr.push(new Food('Watermelon', 'Fruitarian', 300,'Major-fruit-watermelon-96x96.png'));
     foodArr.push(new Food('Turkey', 'Castlevaniesque', 600,'Major-carni-chicken-96x96.png'));
     foodArr.push(new Food('"La Baguette"', 'Baguette', 400,'Major-bag-baguette-96x96.png'));
+    foodArr.push(new Food('Large medkit', 'Half-Life', 1000, 'Medkit.png'));
     foodArr.push(new Food('Camembert', 'Cheese', 300,'Major-cheese-wheel.png'));
     
     let majorFoods = new FoodSection('Major foods', foodArr);
