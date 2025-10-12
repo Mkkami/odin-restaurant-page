fbeb338356dc216e7c6f4f25647818d9a1fd8463
Michał Marciniak <michal.mar725@gmail.com>
2025-10-12 17:55:34 +0200

    add home and about content

--- src/about.js
+++ src/about.js
@@ -3,6 +3,12 @@ export default function generateAbout() {
     content.replaceChildren();
 
     const about = document.createElement('h2');
-    about.textContent = 'This is about page';
+    about.textContent = 'About';
+    
+    const desc = document.createElement('div');
+    desc.textContent = "Dead Cells is a roguelike, Castlevania-inspired action-platformer, allowing you to explore a sprawling, ever-changing castle… assuming you’re able to fight your way past its keepers. To beat the game you’ll have to master 2D souls-like like combat with the ever present threat of permadeath looming. No checkpoints. Kill, die, learn, repeat."
+    desc.classList.add("desc");
+
     content.appendChild(about);
+    content.appendChild(desc);
 }
--- src/home.js
+++ src/home.js
@@ -3,6 +3,12 @@ export default function generateHome() {
     content.replaceChildren();
 
     const home = document.createElement('h2');
-    home.textContent = 'This is home';
+    home.textContent = 'Welcome';
+
+    const desc = document.createElement('div');
+    desc.textContent = "This is a restaurant page inspired by Dead Cells."
+    desc.classList.add("desc");
+
     content.appendChild(home);
+    content.appendChild(desc);
 }
